0eeb05ec3c7fd553026192149acb039094fc764e
Anthony <anthony@meinder.dev>
2024-02-25 16:11:22 +0100

    Annnnd some pylinting...

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """
-This module sets up a Flask web application to check domain DNS configurations (on mail aspects) for domains.
+This module sets up a Flask web application to check
+domain DNS configurations (on mail aspects) for domains.
 """
 
 from flask import Flask, render_template, jsonify
@@ -7,10 +8,12 @@ from dns_check import check_dns
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Render the main page of the web application."""
     return render_template('index.html')
+
 
 @app.route('/dns-check/<domain>')
 def dns_check(domain):
@@ -21,6 +24,7 @@ def dns_check(domain):
     """
     result = check_dns(domain)
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=False)
--- dns_check.py
+++ dns_check.py
@@ -11,6 +11,7 @@ import sys
 import dns.resolver
 import dns.exception
 
+
 @dataclass
 class RecordResult:
     """Class to store individual DNS record check results."""
@@ -19,7 +20,9 @@ class RecordResult:
     score: int
 
     def to_dict(self):
+        """ Needed for serialization """
         return asdict(self)
+
 
 @dataclass
 class RecordsResult:
@@ -33,7 +36,9 @@ class RecordsResult:
     bimi: RecordResult
 
     def to_dict(self):
+        """ Needed for serialization """
         return {key: getattr(self, key).to_dict() for key in self.__dict__}
+
 
 @dataclass
 class DNSCheckResult:
@@ -42,13 +47,17 @@ class DNSCheckResult:
     total_score: int
 
     def to_dict(self):
-        return {"records": self.records.to_dict(), "total_score": self.total_score}
+        """ Needed for serialization """
+        return {
+            "records": self.records.to_dict(),
+            "total_score": self.total_score}
+
 
 common_dkim_selectors = ['default', 'google', 'mail', 'k1', 'smtp']
 
-def dns_query(domain: str,
-              record_type: str,
-              process_func: Callable[[dns.resolver.Answer], RecordResult]) -> RecordResult:
+
+def dns_query(domain: str, record_type: str, process_func: Callable[[
+              dns.resolver.Answer], RecordResult]) -> RecordResult:
     """Perform DNS query and process the results."""
     try:
         answer = dns.resolver.resolve(domain, record_type)
@@ -56,34 +65,51 @@ def dns_query(domain: str,
     except dns.exception.DNSException:
         return RecordResult(None, f"No {record_type} record found", 0)
 
+
 def process_mx(answer: dns.resolver.Answer) -> RecordResult:
     """Process MX record query results."""
     return RecordResult([str(rdata) for rdata in answer], "Found MX record", 1)
 
-def process_a_aaaa(answer: dns.resolver.Answer, record_type: str) -> RecordResult:
+
+def process_a_aaaa(
+        answer: dns.resolver.Answer,
+        record_type: str) -> RecordResult:
     """Process A or AAAA record query results."""
-    return RecordResult([str(rdata) for rdata in answer], f"Found {record_type} record", 1)
+    return RecordResult([str(rdata) for rdata in answer],
+                        f"Found {record_type} record", 1)
+
 
 def process_spf(answer: dns.resolver.Answer) -> RecordResult:
     """Process SPF record query results."""
     spf_records = [str(rdata) for rdata in answer if "v=spf1" in str(rdata)]
-    return RecordResult(spf_records, "Found SPF record", 1 if spf_records else 0)
+    return RecordResult(
+        spf_records,
+        "Found SPF record",
+        1 if spf_records else 0)
+
 
 def process_dmarc(domain: str) -> RecordResult:
     """Process DMARC record query results."""
     try:
         dmarc_answer = dns.resolver.resolve(f"_dmarc.{domain}", "TXT")
-        dmarc_records = [str(rdata) for rdata in dmarc_answer if "v=DMARC1" in str(rdata)]
-        return RecordResult(dmarc_records, "Found DMARC record", 2 if dmarc_records else 0)
+        dmarc_records = [str(rdata)
+                         for rdata in dmarc_answer if "v=DMARC1" in str(rdata)]
+        return RecordResult(
+            dmarc_records,
+            "Found DMARC record",
+            2 if dmarc_records else 0)
     except dns.exception.DNSException:
         return RecordResult(None, "No DMARC record found", 0)
+
 
 def process_dkim(domain: str) -> RecordResult:
     """Attempt to find DKIM records using common selectors."""
     for selector in common_dkim_selectors:
         try:
-            dkim_answer = dns.resolver.resolve(f"{selector}._domainkey.{domain}", "TXT")
-            dkim_records = [str(rdata) for rdata in dkim_answer if "v=DKIM1" in str(rdata)]
+            dkim_answer = dns.resolver.resolve(
+                f"{selector}._domainkey.{domain}", "TXT")
+            dkim_records = [str(rdata)
+                            for rdata in dkim_answer if "v=DKIM1" in str(rdata)]
             if dkim_records:
                 message: str = f"Found DKIM record with selector '{selector}'"
                 return RecordResult(dkim_records, message, 1)
@@ -91,48 +117,71 @@ def process_dkim(domain: str) -> RecordResult:
             continue
     return RecordResult(None, "No DKIM record found with common selectors", 0)
 
+
 def process_bimi(domain: str) -> RecordResult:
     """Check and process BIMI record for the given domain."""
     bimi_record_domain = f"default._bimi.{domain}"
     try:
         answer = dns.resolver.resolve(bimi_record_domain, "TXT")
-        bimi_record = [str(rdata) for rdata in answer if "v=BIMI1" in str(rdata)]
+        bimi_record = [str(rdata)
+                       for rdata in answer if "v=BIMI1" in str(rdata)]
         if bimi_record:
             # Extract SVG URL from the BIMI record
             svg_url = extract_svg_url(bimi_record[0])
-            return RecordResult(bimi_record, f"BIMI record found: <img src=\"{svg_url}\">", 1)
+            return RecordResult(
+                bimi_record, f"BIMI record found: <img src=\"{svg_url}\">", 1)
         return RecordResult(None, "No BIMI record found", 0)
     except dns.exception.DNSException:
         return RecordResult(None, "Failed to fetch BIMI record", 0)
+
 
 def extract_svg_url(bimi_record: str) -> str:
     """Extract the SVG URL from a BIMI record."""
     match = re.search(r'l=([^";]+)', bimi_record)
     return match.group(1) if match else "No SVG URL found"
 
+
 def check_dns(domain: str) -> DNSCheckResult:
     """Check various DNS records for the given domain."""
     dns_check_functions = {
-        'mx': lambda: dns_query(domain, "MX", process_mx),
-        'a': lambda: dns_query(domain, "A", lambda a: process_a_aaaa(a, "A")),
-        'aaaa': lambda: dns_query(domain, "AAAA", lambda a: process_a_aaaa(a, "AAAA")),
-        'spf': lambda: dns_query(domain, "TXT", process_spf),
+        'mx': lambda: dns_query(
+            domain,
+            "MX",
+            process_mx),
+        'a': lambda: dns_query(
+            domain,
+            "A",
+            lambda a: process_a_aaaa(
+                a,
+                "A")),
+        'aaaa': lambda: dns_query(
+            domain,
+            "AAAA",
+            lambda a: process_a_aaaa(
+                a,
+                "AAAA")),
+        'spf': lambda: dns_query(
+            domain,
+            "TXT",
+            process_spf),
         'dmarc': lambda: process_dmarc(domain),
         'dkim': lambda: process_dkim(domain),
-        'bimi': lambda: process_bimi(domain)
-    }
+        'bimi': lambda: process_bimi(domain)}
 
     records_result = {key: func() for key, func in dns_check_functions.items()}
     total_score = sum(result.score for result in records_result.values())
 
-    return DNSCheckResult(records=RecordsResult(**records_result), total_score=total_score)
+    return DNSCheckResult(
+        records=RecordsResult(
+            **records_result),
+        total_score=total_score)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 dns_check.py example.com")
-        exit(-1)
+        sys.exit(-1)
 
-    domain: str = sys.argv[1]
-    result = check_dns(domain)
+    result = check_dns(sys.argv[1])
     jsonData: str = json.dumps(result.to_dict(), indent=4)
     print(jsonData)
